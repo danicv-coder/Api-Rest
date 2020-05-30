8f5c934deba55a3e0c66a904dad1e17bea54950b
unknown <Daniel.calderon.e.v@gmail.com>
2020-05-29 22:15:29 -0300

    Se agrego Cambio de forma de renderizar, elimando el texto de cargar, agregando la clase selected guardando los meals en el formulario y mostrandolos en el servidor

--- gozaloAhora-Web/main.js
+++ gozaloAhora-Web/main.js
@@ -1,15 +1,55 @@
+
+
 const stringToHtml = (s) => {
     const parser = new DOMParser()
     const doc = parser.parseFromString(s,`text/html`)
     return doc.body.firstChild
-    console.log(doc);
 }
 const renderItem = (item) => {
    const element = stringToHtml(`<li data-id="${item._id}">${item.name}</<li>`)
+   element.addEventListener('click', () => {
+       const mealsList = document.getElementById('meals-list')
+       Array.from(mealsList.children).forEach(x => x.classList.remove('selected'))
+       element.classList.add('selected')
+       const mealsIdeinput = document.getElementById('meals-id')
+       mealsIdeinput.value = item._id
+   })
    return element
 }
 
+const renderOrder = (order, meals) => {
+    const meal = meals.find(meal => meal._id === order.meal_id)// "find" sirve para buscar elementos en un arreglo
+    const element = stringToHtml(`<li data-id="${order._id}">${meal.name} - ${order.user_id}</<li>`)
+    return element
+    
+}
+
 window.onload = () => {
+    const orderForm = document.getElementById('order')
+    orderForm.onsubmit = (e) => {
+        e.preventDefault()
+        const mealsId = document.getElementById('meals-id')
+        const mealIdvalue = mealsId.value
+        if(!mealIdvalue){
+            alert("Disculpe! Debe seleccionar un Plato...")
+            return
+        }
+
+        const order = {
+            meal_id: mealIdvalue,
+            user_id: 'Daniel Calderon',
+        }
+
+        fetch('https://serverless-three-eosin.now.sh/api/orders', {
+            method: 'POST',
+            headers: {
+                'content-Type': 'application/json',
+            },
+            body: JSON.stringify(order)
+
+        }).then(x => console.log(x))
+    }
+
     fetch('https://serverless-three-eosin.now.sh/api/meals')
     .then(response => response.json())
     .then(data => {
@@ -20,5 +60,15 @@ window.onload = () => {
         mealsList.removeChild(mealsList.firstElementChild)
         submit.removeAttribute('disabled')// "removeAttribute" me permite selecionar un atributo de un elemento html y eliminarlo
     })//El "método map"() crea un nuevo array con los resultados de la llamada a la función indicada aplicados a cada uno de sus elementos."join()" une todos los elementos de un array formando una cadena y separándolos con aquel argumento que definamos.
+    fetch('https://serverless-three-eosin.now.sh/api/orders')
+    .then(response => response.json())
+    .then(ordersData => {
+        const orderslist = document.getElementById('orders-list')
+        const listOrders = ordersData.map(ordersData => renderOrder(ordersData, data))
+        orderslist.removeChild(orderslist.firstElementChild)
+        listOrders.forEach(element => orderslist.appendChild(element))
+        console.log(ordersData);
+    })
+
     
 }
